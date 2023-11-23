5859af748e3633e68ecae519cfb8c90fbbce77b8
yasinarafatazad <64463942+yasinarafatazad@users.noreply.github.com>
2023-11-23 19:24:06 +0600

    ━━╬٨ـﮩﮩ❤٨ـﮩﮩـ╬━❤️❥༄ᶦᶰᵈAJAD࿐

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var x = 30;
+var y = 50;
+
+var m = "mangoo";
+var n = "orange";
+var o="Are you sure?";
+
+document.getElementById('idd').innerHTML= m;
+document.getElementsByClassName('class')[0].innerHTML = 'mvkhghjgfk';
+
+
+
+
+alert(o);
+
+document.write("Yasin Arafat Ajad");
+document.write(x*y)
+
+console.log("Successfully failed");
+console.log(m+n);
+
